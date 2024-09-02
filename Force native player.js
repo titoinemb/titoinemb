c427a9ec26ef853b080a0d08298b0e41960a96cb
Titoine ⌚️🇫🇷🇨🇦 <123580047+titoinemb@users.noreply.github.com>
2024-09-01 23:17:10 -0400

    Update Force native player.js

--- Force native player.js
+++ Force native player.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name         force player video
+// @description  I am a demo user script that you can safely delete (add any files to this folder and I will no longer automatically generate)
+// @author       Userscripts
+// @version      0.0.1
+// @match        *://*/*
+// @grant        none
+// @inject-into  content
+// ==/UserScript==
 document.addEventListener("DOMContentLoaded", function() {
             // Sélectionner toutes les balises vidéo sur la page
             var videos = document.querySelectorAll('video');
